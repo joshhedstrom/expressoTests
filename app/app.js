62993f3037b13fa93ed37134cd7362b5dfd4c90c
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-14 21:22:56 -0500

    built basic html form

---
+++ app/app.js
@@ -0,0 +1,24 @@
+module.exports = validateForm;
+
+   function validateForm (firstName, lastName, email, userName, password) {
+        if (firstName || lastname || email || userName || password === null) {
+                return 'Please Fill out all fields'
+        }
+
+        if 
+
+    }
+
+const expect = require('chai').expect;
+
+const multiply = function(x, y) {
+}
+describe('#multiply', () => {
+    it('should multiply correctly when given two numbers', () => {
+        expect(multiply(2, 3)).to.equal(6);
+    })
+
+    it('should rturn flase when not given two numbers', () => {
+        expect(multiply('a', 3))
+    })
+})
