610c550743073a92d7fbcb9289dd850ad1eadded
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-18 09:14:43 -0500

    finished tests, removed email regex

--- app/app.js
+++ app/app.js
@@ -2,13 +2,10 @@ module.exports = validateForm;
 
 function validateForm(firstName, lastName, email, userName, password) {
     let passwordTester = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
-    let emailTester = new RegExp('/\S+@\S+/')
     if (!firstName || !lastName || !email || !userName || !password) {
-        return 'Please fill out all fields'
-    } else if (emailTester.test(email)) {
-        return 'Please enter a valid email address'
+        return 'Please fill out all fields';
     } else if (!passwordTester.test(password)) {
-        return 'Please try a stronger password'
+        return 'Please try a stronger password';
     } else {
         return 'passed';
     }
--- test/app.test.js
+++ test/app.test.js
@@ -3,9 +3,19 @@ const expect = require('chai').expect;
 
 
 describe('#validateForm', () => {
-    it('Should pass test', () => {
-        expect(validateForm('firstName', 'lastName', 'email@email.com', 'username', 'asDF345#$')).to.equal('passed');
-    })
-
-    
+    it('Null fields', () => {
+        expect(validateForm('', 'Smith', 'jsmith@email.com', 'jsmith', 'asDF345#$')).to.equal('Please fill out all fields');
+        expect(validateForm('John', '', 'jsmith@email.com', 'jsmith', 'asDF345#$')).to.equal('Please fill out all fields');
+        expect(validateForm('John', 'Smith', '', 'jsmith', 'asDF345#$')).to.equal('Please fill out all fields');
+        expect(validateForm('John', 'Smith', 'jsmith@email.com', '', 'asDF345#$')).to.equal('Please fill out all fields');
+        expect(validateForm('John', 'Smith', 'jsmith@email.com', 'jsmith', '')).to.equal('Please fill out all fields');
+    });
+    it('Invalid passwords', () => {
+        expect(validateForm('John', 'Smith', 'jsmith@email.com', 'jsmith', 'asdfg')).to.equal('Please try a stronger password');
+        expect(validateForm('John', 'Smith', 'jsmith@email.com', 'jsmith', '123456')).to.equal('Please try a stronger password');
+        expect(validateForm('John', 'Smith', 'jsmith@email.com', 'jsmith', '@#$%')).to.equal('Please try a stronger password');
+    });
+    it('All fields valid', () => {
+        expect(validateForm('John', 'Smith', 'jsmith@email.com', 'jsmith', 'asDF345#$')).to.equal('passed');
+    });
 })
