4bb30703f5d1899c9b961f4fd0c7b2665031a744
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-06-18 09:27:13 -0500

    connected routes and added res.send result

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const validateForm = require('./app/app.js')
 module.exports = function(app) {
     app.get("/", (req, res)=>{
         res.sendFile(path.join(__dirname, "./app/public/index.html"));
@@ -6,5 +7,7 @@ module.exports = function(app) {
 
     app.post('/submit', (req, res) => {
         console.log(req.body)
+        let result = validateForm(req.body.firstname, req.body.lastname, req.body.email, req.body.username, req.body.password);
+        res.send(result);
     })
 }
